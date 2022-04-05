9cae967be6dedfd10531dccf85a803e1e1bba354
tightskinbe <49254954+tightskinbe@users.noreply.github.com>
2022-04-04 23:55:59 -0700

    04/04/2022 23:55:59
    Pushed from Powershell Script

--- Final_Scripts/21 Push Github.ps1
+++ Final_Scripts/21 Push Github.ps1
@@ -5,5 +5,5 @@ $GithubApp.AppActivate("GitHub Desktop")
 Start-Sleep -Seconds 0.3
 $Getdate = Get-Date
 $GithubApp.SendKeys("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}$Getdate{TAB}Pushed from Powershell Script{TAB}{ENTER}")
-#Start-Sleep -Seconds 1
-#$GithubApp.SendKeys("")
+Start-Sleep -Seconds 1
+$GithubApp.SendKeys("{TAB}{TAB}{ENTER}")
